17944f4e425d59d6d65af0b8d95ea9c64a31d264
superpuper32 <pochitalin_m@mail.ru>
2020-12-20 01:07:51 +0300

    gendiff modified - arguments & option format added

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,8 @@ import program from 'commander';
 
 program
   .version('0.1.0')
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
   .parse(process.argv);
