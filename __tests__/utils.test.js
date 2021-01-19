5b59463e4c4397300b620bdbaf1d4fb681190873
superpuper32 <pochitalin_m@mail.ru>
2021-01-20 00:00:34 +0300

    tests corrected

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-import genDiff from '../src/index.js';
+import { genDiff } from '../src/utils.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,10 +10,12 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const file1Path = getFixturePath('file1.json');
-const file2Path = getFixturePath('file2.json');
-const expectedFile = readFile('expected_file.txt');
+const file1Path = readFile('file1.json');
+const file2Path = readFile('file2.json');
+const jsonExpected = readFile('expected_file.txt');
 
-test('genDiff compare json files correctly', () => {
-  expect(genDiff(file1Path, file2Path)).toEqual(expectedFile);
+test('genDiff', () => {
+  expect(genDiff(JSON.parse(file1Path), JSON.parse(file2Path))).toBe(
+    jsonExpected,
+  );
 });
