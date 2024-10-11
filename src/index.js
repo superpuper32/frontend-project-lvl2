450530a6f371db6f729abd6b536af4271592790d
superpuper32 <pochitalin_m@mail.ru>
2024-10-11 19:32:24 +0300

    STEP-3: parser refactored

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { genDiff, readFile, getFileType } from './utils.js';
-import parseFile from './parsers.js';
+import parsers from './parsers.js';
 
 export default (filepath1, filepath2) => {
   const content1 = readFile(filepath1);
@@ -8,8 +8,8 @@ export default (filepath1, filepath2) => {
   const content1Type = getFileType(filepath1);
   const content2Type = getFileType(filepath2);
 
-  const parsedFile1 = parseFile(content1, content1Type);
-  const parsedFile2 = parseFile(content2, content2Type);
+  const parsedFile1 = parsers[content1Type](content1);
+  const parsedFile2 = parsers[content2Type](content2);
 
   return genDiff(parsedFile1, parsedFile2);
 };
