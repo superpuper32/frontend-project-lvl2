2478eefd6c6df209f41170071e01d21ff6ee9162
superpuper32 <pochitalin_m@mail.ru>
2024-10-13 21:59:56 +0300

    STEP-6: gen diff tests refactored

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,9 +1,9 @@
 import { fileURLToPath } from 'url';
 import fs from 'fs';
 import path from 'path';
-import yaml from 'js-yaml';
 import { beforeEach } from '@jest/globals';
 
+import parsers from '../src/parsers.js';
 import genDiff from '../src/genDiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -12,6 +12,8 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (name) => path.join(__dirname, '..', '__fixtures__', 'plain', name);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
+const formats = ['json', 'yml'];
+
 let expected;
 
 beforeEach(() => {
@@ -19,16 +21,9 @@ beforeEach(() => {
 });
 
 describe('genDiff compare correctly', () => {
-  test('json files', () => {
-    const f1 = JSON.parse(readFile('f1.json'));
-    const f2 = JSON.parse(readFile('f2.json'));
-
-    expect(genDiff(f1, f2)).toEqual(expected);
-  });
-
-  test('yml files', () => {
-    const f1 = yaml.load(readFile('f1.yml'));
-    const f2 = yaml.load(readFile('f2.yml'));
+  test.each(formats)('%s files', (format) => {
+    const f1 = parsers[format](readFile(`f1.${format}`));
+    const f2 = parsers[format](readFile(`f2.${format}`));
 
     expect(genDiff(f1, f2)).toEqual(expected);
   });
