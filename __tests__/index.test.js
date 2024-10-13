f74273bf412659bd5c28bc1d36a826b957681237
superpuper32 <pochitalin_m@mail.ru>
2024-10-14 00:02:09 +0300

    STEP-7: tests added & formatter

---
+++ __tests__/index.test.js
@@ -0,0 +1,30 @@
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import path from 'path';
+import { beforeEach } from '@jest/globals';
+
+import parsers from '../src/parsers.js';
+import genDiff from '../src/genDiff.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (name) => path.join(__dirname, '..', '__fixtures__', name);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+const formats = ['json', 'yml'];
+
+let expected;
+
+beforeEach(() => {
+  expected = readFile('expected.txt');
+});
+
+describe('genDiff compare correctly', () => {
+  test.each(formats)('%s files', (format) => {
+    const f1 = parsers[format](readFile(`file1.${format}`));
+    const f2 = parsers[format](readFile(`file2.${format}`));
+
+    expect(genDiff(f1, f2)).toEqual(expected);
+  });
+});
--- src/genDiff.js
+++ src/genDiff.js
@@ -1,29 +1,32 @@
 import _ from 'lodash';
 
+import stringify from './formatters.js';
+
 const genDiff = (data1, data2) => {
   const keys1 = _.keys(data1);
   const keys2 = _.keys(data2);
   const keys = _.union(keys1, keys2);
+  const sorted = _.sortBy(keys);
 
   const results = [];
-  keys.forEach((key) => {
+  sorted.forEach((key) => {
     const value1 = _.get(data1, key);
     const value2 = _.get(data2, key);
 
     if (!_.has(data1, key)) {
-      results.push(['  +', `${key}:`, value2]);
+      results.push([` + ${key}`, value2]);
     } else if (!_.has(data2, key)) {
-      results.push(['  -', `${key}:`, value1]);
+      results.push([` - ${key}`, value1]);
     } else if (value1 === value2) {
-      results.push(['   ', `${key}:`, value1]);
+      results.push([`   ${key}`, value1]);
     } else {
-      results.push(['  -', `${key}:`, value1]);
-      results.push(['  +', `${key}:`, value2]);
+      results.push([` - ${key}`, value1]);
+      results.push([` + ${key}`, value2]);
     }
   });
-  results.sort((a, b) => a[1].localeCompare(b[1]));
+//   results.sort((a, b) => (a[0].slice(2)).localeCompare(b[0].slice(2)));
 
-  return `{${'\n'}${results.map((result) => result.join(' ')).join('\n')}${'\n'}}`;
+  return stringify(Object.fromEntries(results));
 };
 
 export default genDiff;
