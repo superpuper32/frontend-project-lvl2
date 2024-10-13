726f741bdd82196fe31f88b00a870542a67b0780
superpuper32 <pochitalin_m@mail.ru>
2024-10-13 23:29:02 +0300

    STEP-7: argument formatter stylish added

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import genDiff from './genDiff.js';
 import parsers from './parsers.js';
 import { readFile, getFileType } from './utils.js';
 
-export default (filepath1, filepath2) => {
-  const content1 = readFile(filepath1);
-  const content2 = readFile(filepath2);
+export default (filepath1, filepath2, formatter) => {
+  const file1 = readFile(filepath1);
+  const file2 = readFile(filepath2);
 
   const type1 = getFileType(filepath1);
   const type2 = getFileType(filepath2);
 
-  const parsed1 = parsers[type1](content1);
-  const parsed2 = parsers[type2](content2);
+  const parsed1 = parsers[type1](file1);
+  const parsed2 = parsers[type2](file2);
 
-  return genDiff(parsed1, parsed2);
+  return genDiff(parsed1, parsed2, formatter);
 };
