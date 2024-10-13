1c04ce062b0ed44fac023302f387243c7aeb521e
superpuper32 <pochitalin_m@mail.ru>
2024-10-13 14:51:28 +0300

    STEP-3: vars renamed

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ export default (filepath1, filepath2) => {
   const content1 = readFile(filepath1);
   const content2 = readFile(filepath2);
 
-  const content1Type = getFileType(filepath1);
-  const content2Type = getFileType(filepath2);
+  const type1 = getFileType(filepath1);
+  const type2 = getFileType(filepath2);
 
-  const parsedFile1 = parsers[content1Type](content1);
-  const parsedFile2 = parsers[content2Type](content2);
+  const parsedFile1 = parsers[type1](content1);
+  const parsedFile2 = parsers[type2](content2);
 
   return genDiff(parsedFile1, parsedFile2);
 };
