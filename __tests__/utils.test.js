ceb15aa8442a20308084d980dcaf135753270d59
superpuper32 <pochitalin_m@mail.ru>
2024-10-17 22:04:42 +0300

    STEP-9: utils lint fixed try 2

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -10,15 +10,17 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('getFileContent return correct ', () => {
-  const filePath = getFixturePath('expected.txt');
-  const expectedFile = fs.readFileSync(getFixturePath('expected.txt'), 'utf-8');
+describe('utils working correctly', () => {
+  test('getFileContent', () => {
+    const filePath = getFixturePath('expected.txt');
+    const expectedFile = fs.readFileSync(getFixturePath('expected.txt'), 'utf-8');
 
-  expect(readFile(filePath)).toBe(expectedFile);
-});
+    expect(readFile(filePath)).toBe(expectedFile);
+  });
 
-test('getFileType return correct extension', () => {
-  expect(getType('/src/index.js')).toBe('js');
-  expect(getType('/db/data.json')).toBe('json');
-  expect(getType('/')).toBe('');
+  test('getFileType', () => {
+    expect(getType('/src/index.js')).toBe('js');
+    expect(getType('/db/data.json')).toBe('json');
+    expect(getType('/')).toBe('');
+  });
 });
