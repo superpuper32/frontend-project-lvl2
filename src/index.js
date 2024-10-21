fc73e3a303dd095f47524b2bbdefd88998e99ee0
superpuper32 <pochitalin_m@mail.ru>
2024-10-21 23:57:30 +0300

    STEP-9: error handling added

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import format from './formatters/index.js';
 import { readFile, getType } from './utils.js';
 
 export default (filepath1, filepath2, formatName = 'stylish') => {
-  const file1 = readFile(filepath1);
-  const file2 = readFile(filepath2);
+  try {
+    const file1 = readFile(filepath1);
+    const file2 = readFile(filepath2);
 
-  const type1 = getType(filepath1);
-  const type2 = getType(filepath2);
+    const type1 = getType(filepath1);
+    const type2 = getType(filepath2);
 
-  const parsed1 = parsers[type1](file1);
-  const parsed2 = parsers[type2](file2);
+    const parsed1 = parsers[type1](file1);
+    const parsed2 = parsers[type2](file2);
+    const diffTree = genDiff(parsed1, parsed2);
 
-  const diffTree = genDiff(parsed1, parsed2);
-
-  return format(diffTree, formatName);
+    return format(diffTree, formatName);
+  } catch (e) {
+    throw new Error('File not recognised!');
+  }
 };
