8a97e60ff31aaa8766486d3bdf5894bb8f9ea142
superpuper32 <pochitalin_m@mail.ru>
2024-10-29 13:06:27 +0300

    STEp-9: fp linter rules fixed & test refactored

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,8 @@
+/* eslint "fp/no-let": "off" */
 import { fileURLToPath } from 'url';
 import fs from 'fs';
 import path from 'path';
+import { beforeEach } from '@jest/globals';
 
 import genDiff from '../index.js';
 
@@ -12,11 +14,19 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8'
 
 const formats = ['json', 'yml'];
 
+let expected; let expectedPlain; let expectedJSON;
+
+beforeEach(() => {
+  expected = readFile('expected.txt');
+  expectedPlain = readFile('expected_plain.txt');
+  expectedJSON = readFile('expected_json.txt');
+});
+
 describe('genDiff compare correctly', () => {
   test.each(formats)('%s files', (format) => {
     const f1 = getFixturePath(`file1.${format}`);
     const f2 = getFixturePath(`file2.${format}`);
-    const expected = readFile('expected.txt');
+    // const expected = readFile('expected.txt');
 
     expect(genDiff(f1, f2)).toEqual(expected);
   });
@@ -24,7 +34,7 @@ describe('genDiff compare correctly', () => {
   test.each(formats)('%s files in plain format', (format) => {
     const f1 = getFixturePath(`file1.${format}`);
     const f2 = getFixturePath(`file2.${format}`);
-    const expectedPlain = readFile('expected_plain.txt');
+    // const expectedPlain = readFile('expected_plain.txt');
     const formatName = 'plain';
 
     expect(genDiff(f1, f2, formatName)).toEqual(expectedPlain);
@@ -33,7 +43,7 @@ describe('genDiff compare correctly', () => {
   test.each(formats)('%s files in json format', (format) => {
     const f1 = getFixturePath(`file1.${format}`);
     const f2 = getFixturePath(`file2.${format}`);
-    const expectedJSON = readFile('expected_json.txt');
+    // const expectedJSON = readFile('expected_json.txt');
     const formatName = 'json';
 
     expect(genDiff(f1, f2, formatName)).toEqual(expectedJSON);
